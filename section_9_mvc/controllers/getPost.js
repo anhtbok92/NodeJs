022361906ffba39f2fad5c9cf74de44647fa551b
Tuan Anh <victory1080@gmail.com>
2020-12-10 15:47:56 +0700

    update something

--- section_9_mvc/controllers/getPost.js
+++
@@ -1,8 +0,0 @@
-const BlogPost = require('../models/BlogPost.js')
-module.exports = (req, res) => {
-    BlogPost.findById(req.params.id, function (error, detailPost) {
-        res.render('post', {
-            detailPost
-        })
-    })
-}
