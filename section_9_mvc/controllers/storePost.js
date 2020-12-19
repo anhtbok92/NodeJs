4eea8ae6a535b04218b95e340e45b48eb26f614e
Tuan Anh <victory1080@gmail.com>
2020-12-19 08:34:11 +0700

    fix bug upload image

--- section_9_mvc/controllers/storePost.js
+++ section_9_mvc/controllers/storePost.js
@@ -2,7 +2,7 @@ const BlogPost = require('../models/BlogPost.js')
 const path = require('path')
 module.exports = (req, res) => {
     let image = req.files.image;
-    image.mv(path.resolve(__dirname, '..', '/public/upload', image.name), function (error) {
+    image.mv(path.resolve(__dirname, '../public/upload', image.name), function (error) {
         BlogPost.create({
             ...req.body,
             image: '/upload/' + image.name
